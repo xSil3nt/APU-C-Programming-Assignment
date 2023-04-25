4d4df2c57a71f5917b95f25ba5a913aae60af361
xSil3nt <ahmedshazin21@gmail.com>
2023-04-25 16:17:49 +0800

    main.c: Add function for main menu

---
+++ main.c
@@ -0,0 +1,16 @@
+//Main Menu function
+void mainMenu() {
+    //Declare var for selection
+    int choice;
+
+    //Show prompt
+    printf("Welcome to the APU Programming Cafe\n");
+    printf("___________________________________\n");
+    printf("1. Login as Admin\n");
+    printf("2. Login as Tutor\n");
+    printf("3. Login as Student\n");
+    printf("4. Exit\n");
+    printf("Enter your choice: ");
+    //Ask for selection
+    scanf("%d", &choice);
+}

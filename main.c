8d2993e22da5aabcb68cad20712ec90f226e40ed
xSil3nt <ahmedshazin21@gmail.com>
2023-04-25 16:20:20 +0800

    main.c: Call functions based on user input

--- main.c
+++ main.c
@@ -13,4 +13,23 @@ void mainMenu() {
     printf("Enter your choice: ");
     //Ask for selection
     scanf("%d", &choice);
+    
+    //Call respective function depending on selection, and return to menu if invalid selection
+    switch (choice) {
+        case 1:
+            adminLogin();
+            break;
+        case 2:
+            tutorLogin();
+            break;
+        case 3:
+            studentLogin();
+            break;
+        case 4:
+            printf("Thank you for using the APU Programming Cafe. Goodbye!\n");
+            exit(0);
+        default:
+            printf("Invalid choice. Please try again.\n");
+            mainMenu();
+    }
 }

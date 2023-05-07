7590b2334eeda5679e65cd3ae5e381e57a94bd88
xSil3nt <ahmedshazin21@gmail.com>
2023-05-07 16:40:32 +0800

    main.c: Implement tutor registration

--- main.c
+++ main.c
@@ -1,224 +1,268 @@
-#include <stdio.h>
-#include <string.h>
-#include <stdlib.h>
+    #include <stdio.h>
+    #include <string.h>
+    #include <stdlib.h>
 
-void main(), mainMenu(), adminLogin(), adminMenu(), tutorLogin(), tutorMenu(), studentLogin(), studentMenu(), regTutor();
+    void main(), mainMenu(), adminLogin(), adminMenu(), tutorLogin(), tutorMenu(), studentLogin(), studentMenu(), regTutor();
 
-//Main Menu function
-void mainMenu() {
-    //Declare var for selection
-    int choice;
+    //Main Menu function
+    void mainMenu() {
+        //Declare var for selection
+        int choice;
 
-    //Show prompt
-    printf("Welcome to the APU Programming Cafe\n");
-    printf("___________________________________\n");
-    printf("1. Login as Admin\n");
-    printf("2. Login as Tutor\n");
-    printf("3. Login as Student\n");
-    printf("4. Exit\n");
-    printf("Enter your choice: ");
-    //Ask for selection
-    scanf("%d", &choice);
-
-    //Call respective function depending on selection, and return to menu if invalid selection
-    switch (choice) {
-        case 1:
-            adminLogin();
-            break;
-        case 2:
-            tutorLogin();
-            break;
-        case 3:
-            studentLogin();
-            break;
-        case 4:
-            printf("Thank you for using the APU Programming Cafe. Goodbye!\n");
-            exit(0);
-        default:
-            printf("Invalid choice. Please try again.\n");
-            mainMenu();
-    }
-}
-
-//Login function for admin, only requires a password, no username needed. Password is hardcoded -xXEpicAdminXx-
-//If correct password is entered, trigger admin menu, otherwise return to login screen
-void adminLogin() {
-    //Declare variable for password input, and prompt for it
-    char password[20];
-    printf("Enter admin password: ");
-    scanf("%s", password);
-    
-    //Check if input is = hardcoded password -xXEpicAdminXx-, strcmp returns 0 if strings match
-    if (strcmp(password, "-xXEpicAdminXx-") == 0) {
-        adminMenu();
-    } else {
-        //Call function again after throwing error if incorrect password is entered
-        printf("Incorrect password. Please try again.\n");
-        adminLogin();
-    }
-}
-
-void adminMenu() {
-     //Declare var for selection
-    int choice;
-    do {
         //Show prompt
-        printf("\nAdmin Menu\n");
-        printf("___________\n");
-        printf("1. Register tutor\n");
-        printf("2. Register student\n");
-        printf("3. Add session\n");
-        printf("4. Enroll student to session\n");
-        printf("5. Display sessions\n");
-        printf("6. Logout\n");
+        printf("Welcome to the APU Programming Cafe\n");
+        printf("___________________________________\n");
+        printf("1. Login as Admin\n");
+        printf("2. Login as Tutor\n");
+        printf("3. Login as Student\n");
+        printf("4. Exit\n");
         printf("Enter your choice: ");
+        //Ask for selection
         scanf("%d", &choice);
 
+        //Call respective function depending on selection, and return to menu if invalid selection
         switch (choice) {
             case 1:
-                //Call function to register tutor
-                regTutor();
+                adminLogin();
                 break;
             case 2:
-                //Call function to register student
+                tutorLogin();
                 break;
             case 3:
-                //Call function to add session
+                studentLogin();
                 break;
             case 4:
-                //Call function to enroll student to session
-                break;
-            case 5:
-                //Call function to display sessions
-                break;
-            case 6:
-                printf("\nLogging out\n");
-                //Return to main menu
-                mainMenu();
-                break;
+                printf("Thank you for using the APU Programming Cafe. Goodbye!\n");
+                exit(0);
             default:
-                printf("\nInvalid choice. Please try again.\n");
-        }
-    } while (choice != 6);
-}
-void tutorLogin() {
-    //Function for logging in tutors
-    //TODO: Maybe commonize tutor and student login? Dunno how much work that will be, decide later?? 
-    //Declare variables for input, 20 characters should be enough
-    //TODO: Maybe declare a const to store max input?
-    char username[20], password[20];
-    //Prompt for input
-    printf("Enter your username: ");
-    scanf("%s", username);
-    printf("Enter your password: ");
-    scanf("%s", password);
-
-    //Open the file that stores tutor credentials, and throw an error if file not found
-    FILE *tutorCreds = fopen("tutorCreds.apdata", "r");
-    if (tutorCreds == NULL) {
-        printf("Error: could not open tutor credentials file.\n");
-        return;
-    }
-
-    //The file stores each set of credentials in a line, # marks the end of the line, commas seperate the credentials by type.
-    //Char array to store lines of the file as we loop through the file to find the password and username
-    char line[100];
-    //Decare pointers to store user and pass found in the file
-    char *user, *pass;
-    
-    //This feels like a confusing way to loop through the file, but I think it's the best
-    //fgets takes 3 arguments, where to store the text, max size of text, and where to read from
-    //The while loop continues until there is no more text, aka, fgets returns NULL when it reaches the end of the file
-    while (fgets(line, sizeof(line), tutorCreds)) {
-        //Get username from the line currently being read, by looking for a comma (called the delimiter)
-        user = strtok(line, ",");
-        //Get password from the line currently being read, looks for hashtag
-        //We use NULL as the first argument because we want strtok to continue checking from the where the previous strtok left off, kinda confusing at first, but it works :p
-        pass = strtok(NULL, "#");
-
-        //Compare entered details with the ones from the file at the current line, if they match, trigger the tutorMenu
-        if (strcmp(username, user) == 0 && strcmp(password, pass) == 0) {
-            printf("\nLogin successful!\n");
-            fclose(tutorCreds);
-            tutorMenu();
+                printf("Invalid choice. Please try again.\n");
+                mainMenu();
         }
     }
-    //We will exit the loop naturally if we don't find matching credentials, so ask the user to try again.
-    printf("\nInvalid username or password. Please try again.\n");
-    fclose(tutorCreds);
-    tutorLogin();
-}
 
-void tutorMenu() {
-    int choice;
-    printf("Tutor Menu\n");
-    printf("___________\n");
-    printf("1. View assigned sessions\n");
-    printf("2. Logout\n");
-    printf("Enter your choice: ");
-    scanf("%d", &choice);
-}
-
-void studentMenu() {
-    int choice;
-    printf("\nStudent Menu\n");
-    printf("____________\n");
-    printf("1. View sessions\n");
-    printf("2. Enroll in a session\n");
-    printf("3. Logout\n");
-    printf("Enter your choice (1-3): ");
-    scanf("%d", &choice);
-}
-
-//Mostly a copy paste from tutorLogin function, should probably commonize but I'm currently a little lazy, perhaps a problem for future me
-void studentLogin() {
-    //Function for logging in students 
-    //Declare variables for input, 20 characters should be enough
-    //TODO: Maybe declare a const to store max input?
-    char username[20], password[20];
-    //Prompt for input
-    printf("Enter your username: ");
-    scanf("%s", username);
-    printf("Enter your password: ");
-    scanf("%s", password);
-
-    //Open the file that stores student credentials, and throw an error if file not found
-    FILE *studentCreds = fopen("studentCreds.apdata", "r");
-    if (studentCreds == NULL) {
-        printf("Error: could not open student credentials file.\n");
-        return;
-    }
-
-    //The file stores each set of credentials in a line, # marks the end of the line, commas seperate the credentials by type.
-    //Char array to store lines of the file as we loop through the file to find the password and username
-    char line[100];
-    //Decare pointers to store user and pass found in the file
-    char *user, *pass;
-    
-    //This feels like a confusing way to loop through the file, but I think it's the best
-    //fgets takes 3 arguments, where to store the text, max size of text, and where to read from
-    //The while loop continues until there is no more text, aka, fgets returns NULL when it reaches the end of the file
-    while (fgets(line, sizeof(line), studentCreds)) {
-        //Get username from the line currently being read, by looking for a comma (called the delimiter)
-        user = strtok(line, ",");
-        //Get password from the line currently being read, looks for hashtag
-        //We use NULL as the first argument because we want strtok to continue checking from the where the previous strtok left off, kinda confusing at first, but it works :p
-        pass = strtok(NULL, "#");
-
-        //Compare entered details with the ones from the file at the current line, if they match, trigger the studentMenu
-        if (strcmp(username, user) == 0 && strcmp(password, pass) == 0) {
-            printf("\nLogin successful!\n");
-            fclose(studentCreds);
-            studentMenu();
+    //Login function for admin, only requires a password, no username needed. Password is hardcoded abc
+    //If correct password is entered, trigger admin menu, otherwise return to login screen
+    void adminLogin() {
+        //Declare variable for password input, and prompt for it
+        char password[20];
+        printf("Enter admin password: ");
+        scanf("%s", password);
+        
+        //Check if input is = hardcoded password abc, strcmp returns 0 if strings match
+        if (strcmp(password, "abc") == 0) {
+            adminMenu();
+        } else {
+            //Call function again after throwing error if incorrect password is entered
+            printf("Incorrect password. Please try again.\n");
+            adminLogin();
         }
     }
-    //We will exit the loop naturally if we don't find matching credentials, so ask the user to try again.
-    printf("\nInvalid username or password. Please try again.\n");
-    fclose(studentCreds);
-    studentLogin();
-}
 
-void main() {
-    //Program starts here
-    mainMenu();
-}
+    void adminMenu() {
+        //Declare var for selection
+        int choice;
+        do {
+            //Show prompt
+            printf("\nAdmin Menu\n");
+            printf("___________\n");
+            printf("1. Register tutor\n");
+            printf("2. Register student\n");
+            printf("3. Add session\n");
+            printf("4. Enroll student to session\n");
+            printf("5. Display sessions\n");
+            printf("6. Logout\n");
+            printf("Enter your choice: ");
+            scanf("%d", &choice);
+
+            switch (choice) {
+                case 1:
+                    //Call function to register tutor
+                    regTutor();
+                    break;
+                case 2:
+                    //Call function to register student
+                    break;
+                case 3:
+                    //Call function to add session
+                    break;
+                case 4:
+                    //Call function to enroll student to session
+                    break;
+                case 5:
+                    //Call function to display sessions
+                    break;
+                case 6:
+                    printf("\nLogging out\n");
+                    //Return to main menu
+                    mainMenu();
+                    break;
+                default:
+                    printf("\nInvalid choice. Please try again.\n");
+            }
+        } while (choice != 6);
+    }
+
+    void regTutor() {
+        //Declare variables for tutor details
+        char tutorId[5], tutorPass[20], tutorName[50], tutorSubject[50];
+
+        //Prompt for tutor details
+        printf("\nEnter tutor ID: ");
+        scanf("%s", tutorId);
+        printf("Enter tutor password: ");
+        scanf("%s", tutorPass);
+        printf("Enter tutor name: ");
+        getchar(); //Remove the newline character left in the input buffer, otherwise newline char is passed to fgets which breaks stuff
+        fgets(tutorName, sizeof(tutorName), stdin);
+        tutorName[strcspn(tutorName, "\n")] = '\0'; //Remove the newline character from the input from when the user presses enter
+        printf("Enter tutor subject: ");
+        fgets(tutorSubject, sizeof(tutorSubject), stdin);
+        tutorSubject[strcspn(tutorSubject, "\n")] = '\0'; //Remove the newline character from the input
+
+        //Check if tutor ID is already in use
+        FILE *tutorCreds = fopen("tutorCreds.apdata", "r");
+        char line[50];
+        while (fgets(line, sizeof(line), tutorCreds)) {
+            char *id = strtok(line, ",");
+            if (strcmp(id, tutorId) == 0) {
+                printf("\nTutor ID already in use. Please try again.\n");
+                fclose(tutorCreds);
+                return;
+            }
+        }
+        fclose(tutorCreds);
+
+        //Add tutor login to creds file
+        tutorCreds = fopen("tutorCreds.apdata", "a");
+        fprintf(tutorCreds, "%s,%s#\n", tutorId, tutorPass);
+        fclose(tutorCreds);
+
+        //Add tutor info to file
+        FILE *tutors = fopen("tutors.apdata", "a");
+        fprintf(tutors, "%s,%s;%s#\n", tutorId, tutorName, tutorSubject);
+        fclose(tutors);
+
+        printf("\nTutor added successfully.\n");
+    }
+
+    void tutorLogin() {
+        //Function for logging in tutors
+        //TODO: Maybe commonize tutor and student login? Dunno how much work that will be, decide later?? 
+        //Declare variables for input, 20 characters should be enough
+        //TODO: Maybe declare a const to store max input?
+        char username[20], password[20];
+        //Prompt for input
+        printf("Enter your username: ");
+        scanf("%s", username);
+        printf("Enter your password: ");
+        scanf("%s", password);
+
+        //Open the file that stores tutor credentials, and throw an error if file not found
+        FILE *tutorCreds = fopen("tutorCreds.apdata", "r");
+        if (tutorCreds == NULL) {
+            printf("Error: could not open tutor credentials file.\n");
+            return;
+        }
+
+        //The file stores each set of credentials in a line, # marks the end of the line, commas seperate the credentials by type.
+        //Char array to store lines of the file as we loop through the file to find the password and username
+        char line[100];
+        //Decare pointers to store user and pass found in the file
+        char *user, *pass;
+        
+        //This feels like a confusing way to loop through the file, but I think it's the best
+        //fgets takes 3 arguments, where to store the text, max size of text, and where to read from
+        //The while loop continues until there is no more text, aka, fgets returns NULL when it reaches the end of the file
+        while (fgets(line, sizeof(line), tutorCreds)) {
+            //Get username from the line currently being read, by looking for a comma (called the delimiter)
+            user = strtok(line, ",");
+            //Get password from the line currently being read, looks for hashtag
+            //We use NULL as the first argument because we want strtok to continue checking from the where the previous strtok left off, kinda confusing at first, but it works :p
+            pass = strtok(NULL, "#");
+
+            //Compare entered details with the ones from the file at the current line, if they match, trigger the tutorMenu
+            if (strcmp(username, user) == 0 && strcmp(password, pass) == 0) {
+                printf("\nLogin successful!\n");
+                fclose(tutorCreds);
+                tutorMenu();
+            }
+        }
+        //We will exit the loop naturally if we don't find matching credentials, so ask the user to try again.
+        printf("\nInvalid username or password. Please try again.\n");
+        fclose(tutorCreds);
+        tutorLogin();
+    }
+
+    void tutorMenu() {
+        int choice;
+        printf("Tutor Menu\n");
+        printf("___________\n");
+        printf("1. View assigned sessions\n");
+        printf("2. Logout\n");
+        printf("Enter your choice: ");
+        scanf("%d", &choice);
+    }
+
+    void studentMenu() {
+        int choice;
+        printf("\nStudent Menu\n");
+        printf("____________\n");
+        printf("1. View sessions\n");
+        printf("2. Enroll in a session\n");
+        printf("3. Logout\n");
+        printf("Enter your choice (1-3): ");
+        scanf("%d", &choice);
+    }
+
+    //Mostly a copy paste from tutorLogin function, should probably commonize but I'm currently a little lazy, perhaps a problem for future me
+    void studentLogin() {
+        //Function for logging in students 
+        //Declare variables for input, 20 characters should be enough
+        //TODO: Maybe declare a const to store max input?
+        char username[20], password[20];
+        //Prompt for input
+        printf("Enter your username: ");
+        scanf("%s", username);
+        printf("Enter your password: ");
+        scanf("%s", password);
+
+        //Open the file that stores student credentials, and throw an error if file not found
+        FILE *studentCreds = fopen("studentCreds.apdata", "r");
+        if (studentCreds == NULL) {
+            printf("Error: could not open student credentials file.\n");
+            return;
+        }
+
+        //The file stores each set of credentials in a line, # marks the end of the line, commas seperate the credentials by type.
+        //Char array to store lines of the file as we loop through the file to find the password and username
+        char line[100];
+        //Decare pointers to store user and pass found in the file
+        char *user, *pass;
+        
+        //This feels like a confusing way to loop through the file, but I think it's the best
+        //fgets takes 3 arguments, where to store the text, max size of text, and where to read from
+        //The while loop continues until there is no more text, aka, fgets returns NULL when it reaches the end of the file
+        while (fgets(line, sizeof(line), studentCreds)) {
+            //Get username from the line currently being read, by looking for a comma (called the delimiter)
+            user = strtok(line, ",");
+            //Get password from the line currently being read, looks for hashtag
+            //We use NULL as the first argument because we want strtok to continue checking from the where the previous strtok left off, kinda confusing at first, but it works :p
+            pass = strtok(NULL, "#");
+
+            //Compare entered details with the ones from the file at the current line, if they match, trigger the studentMenu
+            if (strcmp(username, user) == 0 && strcmp(password, pass) == 0) {
+                printf("\nLogin successful!\n");
+                fclose(studentCreds);
+                studentMenu();
+            }
+        }
+        //We will exit the loop naturally if we don't find matching credentials, so ask the user to try again.
+        printf("\nInvalid username or password. Please try again.\n");
+        fclose(studentCreds);
+        studentLogin();
+    }
+
+    void main() {
+        //Program starts here
+        mainMenu();
+    }
